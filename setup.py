1159323525ef273791cc9330349b91d3ee3e0aee
Pradum03 <86784233+Pradum03@users.noreply.github.com>
2023-05-09 23:40:56 +0530

    Data ingestion

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 from setuptools import find_packages,setup
 from typing import List
+
+HYPEN_E_DOT='-e .'
+
 def get_requirements(file_path:str)->List[str]:
-    requirements= []
+    requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
         return requirements
 
 
+
 setup(
-    name ='DiamondPricepredictiom',
-    version= '0.0.1',
-    author= 'Pradum',
-    author_email= "example@gmail.com",
-    install_requires= get_requirements('requirements.txt'),
-    packages = find_packages()
+    name='DiamondPriceprediction',
+    version='0.0.1',
+    author='Pwskills',
+    author_email="example@gmail.com",
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+
 )
